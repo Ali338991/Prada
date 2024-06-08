ee4bd1f270c20ae5158ae0e87854f3f8d2347b41
Ali338991 <ali.ansari7268@gmail.com>
2024-06-08 22:52:59 +0500

    Add Navbar

---
+++ app/components/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div>
+        <h1 className=' font-bold text-2xl'>Prada</h1>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@nextui-org/react";
 import TopNav from "./components/TopNav";
+import Navbar1 from "./components/Navbar1";
 
 export default function Home() {
   return (
     <div>
-      <TopNav />{" "}
+      <TopNav />
+      <Navbar1/>
     </div>
   );
 }
