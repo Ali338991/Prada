9a7125be8587d5315d98c08dec2b9a8a25657dce
Ali338991 <ali.ansari7268@gmail.com>
2024-06-09 17:15:53 +0500

    all-done-except-footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import { Button } from "@nextui-org/react";
 import TopNav from "./components/TopNav";
 import Navbar1 from "./components/Navbar1";
-
+import HeroSection from "./components/HeroSection";
+import Logos from "./components/Logos";
+import Text from "./components/Text";
+import Products from "./components/Products";
+import TopSeller from "./components/TopSeller";
+import Cards from "./components/Card";
 export default function Home() {
   return (
     <div>
       <TopNav />
       <Navbar1/>
+      <HeroSection/>
+      <Logos/>
+      <Text/>
+      <Products/>
+      <TopSeller/>
+      <Cards/>
     </div>
   );
 }
